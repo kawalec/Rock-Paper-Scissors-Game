9349f59e6086f5b3aefd3f5e05dc077fd87c0f7b
kawalec <kawalec@gmail.com>
2019-05-15 18:53:37 +0200

    result calculation

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const userScore = 0;
-const compScore = 0;
-
 let compChoice = () => {
-  const choice = ["rock", "paper", "scessors"];
+  const choice = ["rock", "paper", "scissors"];
   const random = Math.floor(Math.random() * 3);
   return choice[random];
 };
@@ -12,9 +9,38 @@ const choices = document.querySelector("#choices");
 choices.addEventListener(
   "click",
   function(el) {
-    console.log(el.target.id);
+    let score = el.target.id + compChoice();
+    setScore(score);
   },
   false
 );
 
-console.log(compChoice());
+let userScore = 0;
+let compScore = 0;
+
+let setScore = score => {
+  switch (score) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      console.log(score);
+      userScore += 1;
+      document.querySelector("#user-score").innerHTML = userScore;
+      console.log(userScore);
+      console.log("user +");
+      break;
+    case "rockpaper":
+    case "paperscissors":
+    case "scissorsrock":
+      console.log(score);
+      compScore += 1;
+      document.querySelector("#comp-score").innerHTML = compScore;
+      console.log(compScore);
+      console.log("comp +");
+      break;
+    default:
+      console.log(score);
+      console.log("remis");
+      break;
+  }
+};
